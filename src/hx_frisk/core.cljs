1da73f571df619d7f3320f0a6c5c25108d39c3c8
Will <will@willacton.com>
2018-12-11 09:09:19 -0800

    Create core.cljs

---
+++ src/hx_frisk/core.cljs
@@ -0,0 +1,7 @@
+(ns hx-frisk.core
+  (:require [hx-frisk.shell :as shell]
+            [hx-frisk.view :as view]))
+
+(def DataFriskShell shell/DataFriskShell)
+
+(def DataFriskView view/DataFriskView)
