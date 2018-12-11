34497a6a64a99bf6d0a4c40fe676ab8b5257720d
Will <will@willacton.com>
2018-12-11 09:13:47 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lilactown/hx-frisk "0.0.1"
   :description "Frisking EDN since ~~2016~~ 2018!"
-  :url "https://github.com/Lokeh/hx"
+  :url "https://github.com/Lokeh/hx-frisk"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :source-paths ["src"]
